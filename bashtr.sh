aa9bd77d1615a22c26f08e014ffe36592cbde2bb
Dieudonné MUNGANGA <dieudonneishara@gmail.com>
2022-09-26 10:38:19 +0200

    Bash scripts for linux machines .
    Bash file that Reads the servers.txt line by line and executes traceroute to each of the them .

---
+++ bashtr.sh
@@ -0,0 +1,38 @@
+ #!/bin/bash
+ 
+ #enter info about their network  and location
+ #the info get appended to the final output
+chmod +x bashtr.sh
+list=$(cat "servers.txt")
+var_path=`pwd`
+mkdir -p servers 
+for name in $list
+do
+    echo $name > $var_path/servers/$name
+done
+
+#Traceroute each file and save that to a new file:
+
+for file in $var_path/servers/*
+do
+    name=$(echo ${file##*/})
+    echo "doing " $name
+    traceroute -I $name > $name.txt
+done
+
+ls -1 | grep -v 'servers.txt' | cat *.txt > results.txt
+#ls -1 | grep -v 'servers.txt' | paste -d ';' *.txt > results.csv | paste -d ' ' - -
+# ls -1 | grep -v 'servers.txt' | paste -d ';' *.txt > results.csv | paste -d ';' - -
+
+
+#compare the server IP in the first line, with the server IP in the last line.
+for newfile in *.txt
+do
+    textname=$(echo $newfile)
+    first=$(head -1 $textname | awk '{print $4}' | cut -d ',' -f 1)
+    last=$(tail -1 $textname | awk '{print $3}')
+    if [ $first = $last ]
+    then
+        echo "Yes -" $textname
+    fi
+done
